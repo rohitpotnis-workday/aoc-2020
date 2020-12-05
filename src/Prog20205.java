811f347dc154e6266c5f1b43dc68f0c5017d5cb8
Rohit Potnis <rohit.potnis@workday.com>
2020-12-05 00:48:54 -0800

    day 5 temp checkin

--- src/Prog20205.java
+++ src/Prog20205.java
@@ -2,15 +2,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 /**
- * https://adventofcode.com/2020/leaderboard/day/4
+ * https://adventofcode.com/2020/leaderboard/day/5
  *
  * @author rohitpotnis-workday
+ * //TODO: Try to attempt using bit shift operations
  */
 public class Prog20205 {
 
@@ -20,73 +17,47 @@ public class Prog20205 {
         Path path = Path.of("/Users/rohit.potnis/Downloads/20205_input");
         int maxSeatId = 0;
 
-
-        int [] seatassigned = new int[8*128];
-        Arrays.fill(seatassigned,0);
+        int[] seatassigned = new int[8 * 128];
+        Arrays.fill(seatassigned, 0);
         String[] passes = Files.readString(path).split("\n");
 
-        for (String pass: passes){
-            int row = 0;
+        for (String pass : passes) {
             int lower = 0;
-            int higher= (int) (Math.pow(2,7)-1);
-            for (int i=0; i<pass.length()-3; i++){
-                switch(pass.charAt(i)) {
-                case 'B':{
-                    lower += (higher-lower)/2+1;
-                    row = lower;
+            int higher = (int) (Math.pow(2, 7) - 1);
+            for (int i = 0; i < pass.length() - 3; i++) {
+                switch (pass.charAt(i)) {
+                case 'B': {
+                    lower += (higher - lower) / 2 + 1;
                     break;
                 }
                 case 'F': {
-                    higher -= (higher-lower)/2+1;
-                    row = higher;
+                    higher -= (higher - lower) / 2 + 1;
                 }
 
                 }
             }
             int l = 0;
             int r = 7;
-            int c = 0;
-            for (int i=3; i> 0; i--){
-                switch(pass.charAt(pass.length()-i)) {
-                case 'R':{
-                    l += (r-l)/2+1;
-                    c=l;
+            for (int i = 3; i > 0; i--) {
+                switch (pass.charAt(pass.length() - i)) {
+                case 'R': {
+                    l += (r - l) / 2 + 1;
                     break;
                 }
                 case 'L': {
-                    r -= (r-l)/2+1;
-                    c=r;
+                    r -= (r - l) / 2 + 1;
                 }
-
                 }
-
             }
-            int seatId =  lower*8+l;
-            maxSeatId = Math.max(maxSeatId,seatId);
-            seatassigned[seatId]=1;
-
-            for (int i=0; i<seatassigned.length; i++){
-//                System.out.println(seatassigned[i]);
-            }
-
-
+            int seatId = lower * 8 + l;
+            maxSeatId = Math.max(maxSeatId, seatId);
+            seatassigned[seatId] = 1;
 
         }
-        for (int i=1; i<8*128-1; i++){
-            if (seatassigned[i-1] + seatassigned[i+1]==2 && seatassigned[i]==0){
+        for (int i = 1; i < 8 * 128 - 1; i++) {
+            if (seatassigned[i - 1] + seatassigned[i + 1] == 2 && seatassigned[i] == 0) {
                 System.out.println(i);
             }
         }
-//        System.out.println(maxSeatId);
-
-
-//        Arrays.stream(Files.readString(path).split("\n")).mapToInt(item->item.chars().forEach(
-//            ch->{
-//                int row = 2^7-1;
-//                while (ch!='L' && ch!='R'){
-//                    row = ch=='F'?row/2:row;
-//                }
-//            }));
-//        System.out.println(row);
     }
 }
